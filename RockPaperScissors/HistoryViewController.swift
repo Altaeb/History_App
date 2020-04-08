0a82b50c4d10a7bdc6b2d1bfd1b5ce89928fec2e
aftiyah <abd2010ly@gmail.com>
2020-04-08 18:44:05 +0200

    Add the HistoryViewController

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Abdalfattah Altaeb on 4/8/20.
+//  Copyright © 2020 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
